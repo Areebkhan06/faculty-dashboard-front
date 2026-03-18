136c8a27588cf7a59de55ca9f99bac3f1aa6cb35
Areebkhan06 <areebkhan4575@gmail.com>
2026-03-18 21:35:13 +0530

    delete in mobile and excel upload

--- src/app/dashboard/students/page.js
+++ src/app/dashboard/students/page.js
@@ -500,11 +500,11 @@ export default function StudentsDashboard() {
                   </div>
 
                   <div className="flex gap-2 pt-2">
-                    <button className="flex-1 flex items-center justify-center gap-1 px-3 py-2 text-xs text-slate-700 bg-slate-50 hover:bg-slate-100 rounded transition font-medium">
+                    {/* <button className="flex-1 flex items-center justify-center gap-1 px-3 py-2 text-xs text-slate-700 bg-slate-50 hover:bg-slate-100 rounded transition font-medium">
                       <Eye size={14} /> View
-                    </button>
-                    <button className="flex-1 flex items-center justify-center gap-1 px-3 py-2 text-xs text-slate-700 bg-slate-50 hover:bg-slate-100 rounded transition font-medium">
-                      <Edit size={14} /> Edit
+                    </button> */}
+                    <button onClick={()=>handleDeleteStudent(s._id)} className="flex-1 flex items-center justify-center gap-1 px-3 py-2 text-xs text-slate-700 bg-slate-50 hover:bg-slate-100 rounded transition font-medium">
+                      <Trash2 size={14} /> Delete
                     </button>
                   </div>
                 </div>
